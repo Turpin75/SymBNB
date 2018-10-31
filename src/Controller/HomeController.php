f31cb13626db1c480d50d9e978aba65e244fb9a8
joseph <jturpin99@gmail.com>
2018-10-31 17:43:25 +0100

    Gestion des annonces, formulaire de création et d'édtition

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -8,43 +8,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class HomeController extends AbstractController
 {
-    /**
-     * @Route(
-     * "/hello/{prenom}/{age}", 
-     * name="hello", 
-     * requirements={"prenom":"([a-zA-Z0-9-]+)", "age":"([0-9]+)"}
-     * )
-     *
-     * @return void
-     */
-    public function hello($prenom = "anonyme", $age = 0)
-    {
-        return $this->render(
-            "hello.html.twig",
-            [
-                "prenom" => $prenom,
-                "age" => $age
-            ]
-        );
-        
-    }
-    
-    /**
+ 
+   /**
      * @Route("/", name="homepage")
      */
     public function home()
     {
-        $prenoms = ['Lior' => 31, 'Joseph' => 30, 'Anne' => 55];
-
-        return $this->render
-        (
-            "home.html.twig", 
-            [
-                "title" => "Bonjour tout le monde !", 
-                "age" => 17,
-                "prenoms" => $prenoms
-            ]
-        );
+        return $this->render("home.html.twig");
     }
 }
 
---
+++ src/Form/AnnonceType.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Ad;
+use App\Form\ImageType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\UrlType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\MoneyType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\CollectionType;
+
+class AnnonceType extends AbstractType
+{
+    /**
+     * Permet d'avoir la configuration de base d'un champ
+     *
+     * @param string $label
+     * @param string $placeholder
+     * @return array
+     */
+    private function getConfiguration($label, $placeholder)
+    {
+        $array = [
+                    'label' => $label,
+                    'attr' => ['placeholder' => $placeholder]
+                 ];
+
+        return $array;
+    }
+    
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('title', TextType::class, $this->getConfiguration("Titre", 
+            "Saisissez le titre de l'annonce"))
+            ->add("coverImage", UrlType::class, $this->getConfiguration("Url de l'image principale", 
+            "Saisissez l'url de l'image liée a l'annonce"))
+            ->add("introduction", TextType::class, $this->getConfiguration("Introduction", 
+            "Donnez une description globale de l'annonce"))
+            ->add("content", TextareaType::class, $this->getConfiguration("Desciption", 
+            "Donnez une description détaillée de l'annonce"))
+            ->add('rooms', IntegerType::class, $this->getConfiguration("Nombre de chambres", 
+            "Saisissez le nombre de chambres disponibles"))
+            ->add("price", MoneyType::class, $this->getConfiguration("Prix par nuit", 
+            "Indiquez le prix pour une nuit"))
+            ->add('images', CollectionType::class,
+            [
+                'entry_type' => ImageType::class,
+                'allow_add' => true,
+                'allow_delete' => true,
+                'prototype' => true,
+                'by_reference' => false
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Ad::class,
+        ]);
+    }
+}
